eabac03c136487ac3136744a03deeacff5824b89
Tommy Ho <64376417+tomho7492@users.noreply.github.com>
2020-06-30 22:39:14 -0500

    Add files via upload

--- interface.py
+++ interface.py
@@ -1,7 +1,15 @@
 import datetime
 
-def interface(hashTable):
+def generatePackageMessage(package, hasBeenDelivered):
+    if hasBeenDelivered:
+        print(f"ID: {package.ID} \nDelivery Address: {package.address} \nDelivery Deadline: {package.deadline} \nDelivery City: {package.city}"
+        f"\nDelivery Zipcode: {package.zip} \nPackage Weight: {package.weightInKilos} \nDelivery Status: {package.deliveryStatus}  \nDelivery Time: {package.deliveryTime}")
+    else:
+       print(f"ID: {package.ID} \nDelivery Address: {package.address} \nDelivery Deadline: {package.deadline} \nDelivery City: {package.city}"
+        f"\nDelivery Zipcode: {package.zip} \nPackage Weight: {package.weightInKilos} \nDelivery Status: En route")
 
+
+def interface(hashTable):
     num = int(input(" \n type 2 to view status of package \n type 3 to view all packages"))
 
     if num == 2:
@@ -11,12 +19,9 @@ def interface(hashTable):
         time = datetime.time(hour=int(h), minute=int(m),second=int(s))
         package = hashTable.search(id)
         if time > package.deliveryTime:
-            print(f"ID: {package.ID} \nDelivery Address: {package.address} \nDelivery Deadline: {package.deadline} \nDelivery City: {package.city}"
-            f"\nDelivery Zipcode: {package.zip} \nPackage Weight: {package.weightInKilos} \nDelivery Status: {package.deliveryStatus}  \nDelivery Time: {package.deliveryTime}")
+            generatePackageMessage(package, True)
         else:
-            print(
-                f"ID: {package.ID} \nDelivery Address: {package.address} \nDelivery Deadline: {package.deadline} \nDelivery City: {package.city}"
-            f"\nDelivery Zipcode: {package.zip} \nPackage Weight: {package.weightInKilos} \nDelivery Status: En route")
+            generatePackageMessage(package, False)
     if num == 3:
         time = (input("input a time in the format of HH:MM:SS"))
         (h,m,s) = time.split(':')
@@ -25,13 +30,9 @@ def interface(hashTable):
         for list in hashTable.table:
             package = list[0]
             if time > package.deliveryTime:
-                print(
-                    f"ID: {package.ID} \nDelivery Address: {package.address} \nDelivery Deadline: {package.deadline} \nDelivery City: {package.city}"
-                    f"\nDelivery Zipcode: {package.zip} \nPackage Weight: {package.weightInKilos} \nDelivery Status: {package.deliveryStatus}  \nDelivery Time: {package.deliveryTime}")
+                generatePackageMessage(package, True)
             else:
-                print(
-                    f"ID: {package.ID} \nDelivery Address: {package.address} \nDelivery Deadline: {package.deadline} \nDelivery City: {package.city}"
-                    f"\nDelivery Zipcode: {package.zip} \nPackage Weight: {package.weightInKilos} \nDelivery Status: En route")
+                generatePackageMessage(package, False)
 
 def completedTotalMilesInterface(totalMiles):
     print(f"========================================================"
